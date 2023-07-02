822860cbf591b0e61133ce7a9a5cfc85247ecc98
santhosh <santhoshk621@gmail.com>
2023-07-02 14:50:02 +0530

    first commit

---
+++ src/demo12345/democlasses.java
@@ -0,0 +1,21 @@
+package demo12345;
+
+public class democlasses {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("gtyguiujuihyuu");
+		System.out.println("gtyguiujuihyuu");
+		System.out.println("gtyguiujuihyuu");
+		System.out.println("gtyguiujuihyuu");
+		System.out.println("gtyguiujuihyuu");
+		System.out.println("gtyguiujuihyuu");
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
