225ba3b67b99eb9e2e1b9e066570617c1d7ebfb5
santhosh <santhoshk621@gmail.com>
2023-07-02 14:57:08 +0530

    geewegydgy

--- src/demo12345/democlasses.java
+++ src/demo12345/democlasses.java
@@ -12,6 +12,23 @@ public class democlasses {
 		System.out.println("gtyguiujuihyuu");
 		System.out.println("gtyguiujuihyuu");
 		
+		int y=2;
+		try
+		{		int z=9/0;
+
+		
+		//throw New ArithamaticException();
+		}
+		catch(Exception e) {
+
+			System.out.println(e.getMessage());
+		System.out.println(e.toString());
+		}
+		catch(NoClassDefFoundError e) {
+			
+			System.out.println(e);
+		}
+		
 		
 		
 		// TODO Auto-generated method stub
